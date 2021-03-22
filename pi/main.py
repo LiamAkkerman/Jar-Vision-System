149f035541c002adea7f728fd08b0458244c4a89
liam <liam.akkerman@gmail.com>
2021-03-22 15:40:24 -0700

    debug for rpi
    bug fixes because I can test things on hardware now.
    - is to == when needed
    - library fixes
    - syntax errors

--- pi/main.py
+++ pi/main.py
@@ -7,8 +7,9 @@ import sys
 from picamera import PiCamera
 from gpiozero import Button
 
-import tensorflow as tf
-from tensorflow import keras
+# import tensorflow as tf
+# from tensorflow import keras
+import tflite_runtime as tflite
 
 # metadata
 __copyright__ = 'Copyright 2021'
@@ -27,7 +28,7 @@ def capture_image(camera, save=True, path='/home/pi/images'):
     """
 
     filename = 'foo.rgb' #TODO change to increment or something
-    camera.capture(filename, resize=(320, 320), 'rgb')
+    camera.capture(filename, 'rgb', resize=(320, 320))
     print(filename, 'captured')
 
     if save:
@@ -44,7 +45,7 @@ def run_model(model): #there is probably a conventional name for this
     coordinates = (0, 0) #TODO, obviously
     return coordinates
 
-def display_result()
+def display_result():
     #TODO I don't know how this will happen besides printing the coordinates to the console
     # a web interface could be made which overlays a circle on the image but that is over-zealous
     return None
@@ -52,18 +53,23 @@ def display_result()
 
 
 # main program
-if __name__ is '__main__':
+if __name__ == '__main__':
     print('Jar Detection System. Version', __version__)
 
+    # get command line options and parameters
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
     args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
+    # initialize camera
     camera = PiCamera()
     camera.resolution = (720, 720) #TODO set the correct res and other settings
     sleep(2) # camera start up time
     print('camera initialised')
 
-    button = Button(4) #button across GPIO4 to GND
+    # initialize the hardware button
+    # the button is used to take a photo and run the model
+    # button across GPIO4 to GND
+    button = Button(4) 
 
     # default options
     model = None
@@ -74,12 +80,13 @@ if __name__ is '__main__':
     # parse command line options
     while opts:
         opt = opts.pop()
-        if opt is '--collect':
+        print('option:', opt)
+        if opt == '--collect':
             print('data collection mode, no model will run')
             collect_mode = True
             continue
 
-        if opt is '--dry':
+        if opt == '--dry':
             print('dry run mode, not images will be saved')
             save_mode = False
             continue
@@ -89,7 +96,7 @@ if __name__ is '__main__':
             print('upload path specified:', up_path)
             continue
 
-        print('ERROR: unknown option,' opt)
+        print('ERROR: unknown option', opt)
         raise(KeyError)
 
     # load Keras model (if required)
@@ -97,18 +104,18 @@ if __name__ is '__main__':
         model = keras.models.load_model('/home/pi/model.model') #TODO error handling, TODO pass model in args
         print('keras model loaded')
 
+    print('\nready!')
     # main loop
     while True:
         button.wait_for_press()
         capture_image(camera=camera, save=save_mode, path=up_path)
 
-        if model is None:
-            # collect mode
+        if collect_mode:
             sleep(2)
         else:
             preprocess_image()
             run_model(model=model) 
     
     camera.close()
-    return None # this will never be reached. I'm not sure how this should be for python
+    #return None # this will never be reached. I'm not sure how this should be for python
 
