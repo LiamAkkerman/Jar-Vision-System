e6aaa6a2c41fe1a00064340779aee461c671cef3
liam <liam.akkerman@gmail.com>
2021-04-23 21:35:31 -0700

    add demo files

--- demo/demo.py
+++ demo/demo.py
@@ -4,7 +4,7 @@ import tensorflow.lite as tflite
 from glob import glob
 
 
-interpreter = tflite.Interpreter(model_path='./demo/model.tflite')
+interpreter = tflite.Interpreter(model_path='./model_202104240144.tflite')
 print('interpreter loaded', type(interpreter))
 
 interpreter.allocate_tensors()
@@ -14,7 +14,7 @@ output_details = interpreter.get_output_details()
 
 
 images = glob('./demo/*.npy')
-for image_name in images:
+for i, image_name in enumerate(images):
     image = np.asarray(np.load(image_name, allow_pickle=True)/255.0, dtype=np.float32)
     image_cropped = image.copy()[79:579, 236:736, :] # crop image
     image_sized = np.resize(image_cropped, (1, *image_cropped.shape))
@@ -26,26 +26,25 @@ for image_name in images:
     pred = overlay_pred.copy()
     overlay_pred[overlay_pred < 0.1] = None
 
-    fig, ax = plt.subplots()
-    ax.imshow(image_cropped)
-    plt.xlim([0, 500])
-    plt.ylim([0, 500])
-    plt.title('Input Image')
+    fig, ax = plt.subplots(nrows=3, sharex=True, figsize=(4, 12))
 
-    fig, ax = plt.subplots()
-    ax.imshow(image_cropped)
-    ax.imshow(overlay_pred, cmap='Reds', alpha=0.6)
-    plt.xlim([0, 500])
-    plt.ylim([0, 500])
-    plt.title('Input Image with Predicted Overlay')
+    ax[0].imshow(image_cropped)
+    ax[0].set_xlim([0, 500])
+    ax[0].set_ylim([0, 500])
+    ax[0].set_title('Input Image')
 
-    fig, ax = plt.subplots()
-    ax.imshow(pred, cmap='Reds')
-    plt.xlim([0, 500])
-    plt.ylim([0, 500])
-    plt.title('Model Prediction')
+    ax[1].imshow(image_cropped)
+    ax[1].imshow(overlay_pred, cmap='Reds', alpha=0.6)
+    ax[1].set_xlim([0, 500])
+    ax[1].set_ylim([0, 500])
+    ax[1].set_title('Input Image with Predicted Overlay')
 
-    plt.show()
+    ax[2].imshow(pred, cmap='Reds')
+    ax[2].set_xlim([0, 500])
+    ax[2].set_ylim([0, 500])
+    ax[2].set_title('Model Prediction')
+
+    plt.savefig('./demo/image_' + str(i) + '.png', format='png')
     
 
 
